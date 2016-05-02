a2323d3b10c118bb83d1238a0f0351a673a4fd21
ganjikar <ganjikar@paypal.com>
2016-05-02 09:21:24 -0400

    Filed added to the project to be merged

---
+++ com/SecondDemoProgram.java
@@ -0,0 +1,9 @@
+/**
+ * SecondDemoProgram
+ */
+public class SecondDemoProgram {
+
+    public static void main(String[] args) {
+        System.out.print("My second program");
+    }
+}
---
+++ dir2/src/DemoProgram.java
@@ -0,0 +1,11 @@
+package src;
+
+/**
+ * DemoProgram
+ */
+public class DemoProgram {
+
+    public static void main(String[] args) {
+        System.out.print("Another demo program");
+    }
+}
